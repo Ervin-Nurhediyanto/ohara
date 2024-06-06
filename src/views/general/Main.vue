e971fc602ec6e440995cd9af9835ae0dd02ce66e
Ervin Nurhediyanto <cafeinai311@gmail.com>
2024-06-06 23:47:51 +0700

    init

--- src/views/general/Main.vue
+++ src/views/general/Main.vue
@@ -3,21 +3,39 @@
     <Navbar v-on:handleMenuBar="handleMenuBar"/>
     <router-view class="shadow-sm"/>
     <Footer/>
+    <div class="music-player">
+      <!-- <audio
+        ref="audio"
+        :src="bgm"
+        preload
+        loop
+        id="audio"
+      ></audio> -->
+      <!-- <div @click="toggleSound()" class="toggle-sound"></div> -->
+    </div>
   </div>
 </template>
 
 <script>
+import sound from '../../assets/bgm/bgm-home.mp3'
 import Navbar from '../../components/navbar/Main.vue'
 import Footer from '../../components/footer/Main.vue'
 
 export default {
   name: 'Main-General',
   data () {
-    return {}
+    return {
+      sound: sound
+    }
   },
   components: {
     Navbar,
     Footer
+  },
+  mounted () {
+    const audio = new Audio(this.sound)
+    audio.play()
+    audio.loop = true
   },
   methods: {}
 }
